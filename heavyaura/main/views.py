6a870833393f3b2eac8c86c041a99e5c93de9e18
e.Lyalkov <e_lyalkov@mail.ru>
2025-06-25 14:32:32 +0300

    Написали представления popular_list и product_detail

--- heavyaura/main/views.py
+++ heavyaura/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+#render — функция, которая позволяет взять HTML-шаблон и вставить в него данные, а затем вернуть готовую HTML-страницу пользователю
+#get_object_or_404 — функция, которая ищет объект в базе данных. Если объект не найден, она автоматически возвращает ошибку
+def popular_list(request):
+    products = Product.objects.filter(available=True)[:3]
+    return render(request,
+                  'main/index/index.html',
+                  {'products': products}) #в словарь передаем значения, которые будем вызывать в html-шаблоне (for...in)
 
-# Create your views here.
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'main/product/detail.html',
+                  {'product': product})
+
