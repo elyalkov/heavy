4a1c929f000c7a9d884dd00704ba026f450edfbd
e.Lyalkov <e_lyalkov@mail.ru>
2025-06-27 10:39:12 +0300

    Добавили пагинацию

--- heavyaura/main/views.py
+++ heavyaura/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Product, Category
 #render — функция, которая позволяет взять HTML-шаблон и вставить в него данные, а затем вернуть готовую HTML-страницу пользователю
 #get_object_or_404 — функция, которая ищет объект в базе данных. Если объект не найден, она автоматически возвращает ошибку
@@ -19,15 +20,21 @@ def product_detail(request, slug):
 
 
 def product_list(request, category_slug=None):
+    page= request.GET.get('page', 1)
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 10)
+    current_page = paginator.page(int(page))
     if category_slug:
         category = get_object_or_404(Category,
                                      slug=category_slug)
-        products = products.filter(category=category)
+        paginator = Paginator(products.filter(category=category), 10)
+        #products = products.filter(category=category) удалили после добавления пагинатора
+        current_page = paginator.page(int(page))
     return render(request,
                   'main/product/list.html',
                   {'category': category,
                    'categories': categories,
-                   'products': products})
+                   'products': current_page, #products заменили на current_page после пагинатора
+                   'slug_url': category_slug})
